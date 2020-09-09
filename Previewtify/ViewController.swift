ea8d3c2a3c5e2d45c933a03619df1b93b32a985f
Samuel Folledo <samuelfolledo@gmail.com>
2020-09-09 19:26:59 -0400

    [add] initialize project

---
+++ Previewtify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Previewtify
+//
+//  Created by Samuel Folledo on 9/9/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
