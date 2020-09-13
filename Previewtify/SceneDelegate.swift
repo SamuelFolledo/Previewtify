8f883bfcd1e94edcc1eaef751bc00398b1acb692
Samuel Folledo <samuelfolledo@gmail.com>
2020-09-13 12:22:18 -0400

    [add] made authentication and authorization to work

--- Previewtify/SceneDelegate.swift
+++ Previewtify/SceneDelegate.swift
@@ -11,13 +11,38 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    lazy var rootViewController = ViewController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.makeKeyAndVisible()
+        window!.windowScene = windowScene
+        window!.rootViewController = rootViewController
+    }
+    
+    //Added for spotify authentication
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+
+        let parameters = rootViewController.appRemote.authorizationParameters(from: url)
+
+        if let code = parameters?["code"] {
+            print("Got Code \(parameters)")
+            rootViewController.responseTypeCode = code
+        } else if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            self.rootViewController.accessToken = access_token
+        } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            // Show the error
+            print("No access token error =", error_description)
+        }
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        rootViewController.sessionManager.application(app, open: url, options: options)
+        return true
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -30,11 +55,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        if let accessToken = rootViewController.appRemote.connectionParameters.accessToken {
+            rootViewController.appRemote.connect()
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        if (rootViewController.appRemote.isConnected) {
+            rootViewController.appRemote.disconnect()
+        }
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
@@ -47,7 +78,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
+extension SceneDelegate: SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+      print("Spotify connected")
+    }
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+      print("Spotify disconnected")
+    }
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+      print("Spotify failed")
+    }
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+      print("Spotify player state changed")
+    }
+}
