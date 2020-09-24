92e0585bef775046d01aa7426c03336205be61d4
Samuel Folledo <samuelfolledo@gmail.com>
2020-09-24 18:54:25 -0400

    [add] made SpotifyAuth singleton

--- Previewtify/Models/SpotifyAuth.swift
+++ Previewtify/Models/SpotifyAuth.swift
@@ -11,9 +11,41 @@ import Foundation
 struct SpotifyAuth {
     public let tokenType: String //Bearer
     public let refreshToken: String?
-    public let accessToken: String
-    public let expiresIn: Int
-    public let scope: String
+    public var accessToken: String
+    public var expiresIn: Int
+    public var scope: String
+    
+    //MARK: Singleton
+    private static var _current: SpotifyAuth?
+    static var current: SpotifyAuth? {
+        // Check if current user (tenant) exist
+        if let currentSpotifyAuth = _current {
+            return currentSpotifyAuth
+        } else {
+            // Check if the user was saved in UserDefaults. If not, return nil
+            guard let spotifyAuth = UserDefaults.standard.getStruct(SpotifyAuth.self, forKey: Constants.spotifyAuthKey) else { return nil }
+            _current = spotifyAuth
+            return spotifyAuth
+        }
+    }
 }
 
 extension SpotifyAuth: Codable { }
+
+// MARK: - Static Methods
+extension SpotifyAuth {
+    static func setCurrent(_ spotifyAuth: SpotifyAuth, writeToUserDefaults: Bool = false) {
+        // Save user's information in UserDefaults excluding passwords and sensitive (private) info
+        if writeToUserDefaults {
+            UserDefaults.standard.setStruct(spotifyAuth, forKey: Constants.spotifyAuthKey)
+        }
+        _current = spotifyAuth
+    }
+    
+    static func removeCurrent(_ removeFromUserDefaults: Bool = false) {
+        if removeFromUserDefaults {
+            Defaults._removeSpotifyAuth(true)
+        }
+        _current = nil
+    }
+}
