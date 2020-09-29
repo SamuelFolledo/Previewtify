7a2cd5b8c50d7f8a2e575463eddce0bdb41ce659
Samuel Folledo <samuelfolledo@gmail.com>
2020-09-29 03:03:08 -0400

    [add] track controller

---
+++ Previewtify/Controllers/ArtistTrackController.swift
@@ -0,0 +1,28 @@
+//
+//  ArtistTrackController.swift
+//  Previewtify
+//
+//  Created by Samuel Folledo on 9/29/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+import Spartan
+
+class ArtistTrackController: UIViewController {
+    
+    //MARK: Properties
+    var artist: Artist!
+    
+    //MARK: Views
+    
+    
+    //MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Private Methods
+    
+    //MARK: Helpers
+}
